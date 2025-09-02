9c4cca2fd52159f3b55728db605ae57d96aa9171
vivekanandan-ks <ksvdevksv@gmail.com>
2025-09-03 01:48:40 +0530

    added most configs from pc have to test it out one by one

--- home.nix
+++ home.nix
@@ -1,4 +1,10 @@
-{ config, lib, pkgs, inputs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
   # Read the changelog before changing this value
@@ -7,26 +13,57 @@
   # insert home-manager config
   #nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
 
+  imports = [
+    #./homeModules/shells.nix
+		#./homeModules/terminal-tools-hm.nix
+		#./homeModules/cli-apps-hm.nix
+
+  ];
+
   home.packages = with pkgs; [
-	gh
-	#git
-	nano
-	git-town
-	#rip2 # NA - not available in nix-on-droid
-	tldr
-	man
-	bat	
+    gh
+    #git
+    nano
+    #git-town
+    #rip2 # NA - not available in nix-on-droid
+    tldr
+    man
+    bat
 
-  ]; 
+  ];
 
-  programs.git = {
-	enable = true;
-	package = pkgs.git;
-	extraConfig = {
-		user.name = "vivekanandan-ks";
-		user.email = "ksvdevksv@gmail.com";
-		init.defaultBranch = "main";
-	};	
+  home-manager.autoExpire = {
+    enable = true;
+    frequency = "daily";
+    timestamp = "-3 days";
   };
+
+  home.sessionVariables = {
+    #EDITOR = "micro"; # micro added in cli-apps-hm.nix file
+    #MANPAGER = "sh -c 'col -b | bat -l man -p '"; # add -p flag to bat for plain style
+
+  };
+
+  home.shellAliases = {
+    #rm = "echo Use 'rip' instead of rm.";
+    #rip = "rip --graveyard ~/.local/share/Trash/files/rip2trash";
+    #df = "echo try duf instead";
+    #grep = "echo 'try ripgrep or ripgrep-all and rg or rga is the command'";
+    #man = "batman";
+
+  };
+
+  /*programs = {
+    git = {
+      enable = true;
+      package = pkgs.git;
+      extraConfig = {
+        user.name = "vivekanandan-ks";
+        user.email = "ksvdevksv@gmail.com";
+        init.defaultBranch = "main";
+      };
+    };
+
+  };*/
 
 }
---
+++ homeModules/cli-apps-hm.nix
@@ -0,0 +1,74 @@
+{
+  #inputs,
+  #config,
+  #lib,
+  pkgs,
+  ...
+}:
+
+{
+
+  /*#btop
+  programs.btop = {
+    enable = true;
+    package = pkgs.btop;
+  };
+
+  # bat # cat modern alternative
+  programs.bat = {
+    enable = true;
+    package = pkgs.bat;
+    extraPackages = with pkgs.bat-extras; [
+      batman
+    ];
+
+  };
+
+  # fastfetch
+  programs.fastfetch = {
+    enable = true;
+    package = pkgs.fastfetch;
+    settings = builtins.fromJSON (builtins.readFile ./hmResources/fastfetch-settings.json);
+  };*/
+
+  # git
+  programs.git = {
+    enable = true;
+    package = pkgs.git;
+    extraConfig = {
+      user.name = "vivekanandan-ks";
+      user.email = "ksvdevksv@gmail.com";
+      init.defaultBranch = "main";
+      #core.editor = "nano";
+    };
+  };
+
+  # jujutsu
+  /*
+    programs.jujutsu = {
+      enable = true;
+      package = pkgs-unstable.jujutsu;
+      settings = {
+        user = {
+          email = "ksvdevksv@gmail.com";
+          name = "vivekanandan-ks";
+        };
+        #ui.editor = "micro";
+        snapshot.max-new-file-size = "10MiB"; # https://github.com/jj-vcs/jj/blob/main/docs/config.md#maximum-size-for-new-files
+      };
+    };
+  */
+
+  /*
+    # github
+    programs.gh = {
+      enable = true ;
+      package = pkgs-unstable.gh;
+      #gitCredentialHelper = {
+        #enable = true ;
+        #hosts = [];
+      #};
+    };
+  */
+
+}
